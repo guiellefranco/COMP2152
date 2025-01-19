6a74d543638cc386cec86ce2b1d40acba2a74300
Khaila Franco <guielle.franco@gmail.com>
2025-01-19 16:41:02 -0500

    Adding lab week 1 file

---
+++ labs/week01/lab1.py
@@ -0,0 +1,30 @@
+# Sample Coding Questions 01 Week 01
+# First Name: Khaila, Last Name: Franco
+
+# Defining an array
+array_1 = [1, 4, 7, 9]
+
+# Order of Operations
+a = 1
+b = 2
+c = 3
+d = 4
+
+# Given expression
+e = (a - ((b ** c) // d) + (a % c))
+
+# Formatting
+temperature = 32.6
+
+# Print
+print("The temperature today is: {:.3f} degrees Celsius".format(temperature))
+
+# Common Functions
+# Ask user to input their age
+userAge = int(input("Please enter your age: "))
+
+# Add 22 to the input age
+userAge += 22
+
+# Print the sentence with the updated age
+print(f"Now showing the shop items filtered by age: {userAge}")
